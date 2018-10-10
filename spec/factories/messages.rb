b7c10c8c7f06b7e2f093c54fa843082cdcc5d6b1
mitsuyoshi <m123matsuo@icloud.com>
2018-10-10 09:46:33 +0900

    Rename FactryBot

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
+    image File.open("#{Rails.root}/spec/fixtures/530713_m.jpg")
     user
     group
   end
